18cbdd0b9e67ca7aab7b7487bd1acfd0fc298db7
Duc Nguyen <dbackspace.hust@gmail.com>
2020-12-02 09:22:21 +0700

    Add finish splash

--- app/src/main/java/com/xlteam/wordmatching/ui/SplashActivity.java
+++ app/src/main/java/com/xlteam/wordmatching/ui/SplashActivity.java
@@ -11,5 +11,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
