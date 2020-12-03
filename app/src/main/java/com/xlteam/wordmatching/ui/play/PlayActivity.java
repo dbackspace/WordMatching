c639eb32e539f194b4149c78264cebe0081cde77
binhnk08 <39753132+binhnk08@users.noreply.github.com>
2020-12-03 17:30:26 +0700

    Add files via upload

--- app/src/main/java/com/xlteam/wordmatching/ui/play/PlayActivity.java
+++ app/src/main/java/com/xlteam/wordmatching/ui/play/PlayActivity.java
@@ -1,16 +1,59 @@
 package com.xlteam.wordmatching.ui.play;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.xlteam.wordmatching.R;
+import com.xlteam.wordmatching.database.DBController;
+
+import java.util.HashSet;
 
 public class PlayActivity extends AppCompatActivity {
+    ImageView imgSupport, imgSend;
+    EditText edtInputWord;
+    RecyclerView rvWord;
+    WordAdapter mWordAdapter;
+    HashSet<String> setData;
+    DBController dbController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        setData = new HashSet<>(); //get data cũ hoặc khởi tạo
+        dbController = new DBController(this);
+        dbController.createDatabase();
+        dbController.open(); // tối về cho cái của nợ này vào controller cả
+        findViewById();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvWord.setLayoutManager(linearLayoutManager);
+        mWordAdapter = new WordAdapter(setData, 2, 0);
+        rvWord.setAdapter(mWordAdapter);
+
+        imgSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = edtInputWord.getText().toString();
+                if (!setData.contains(word) && dbController.checkWordInDB(word)) {
+                    setData.add(word);
+                    String wordBot = dbController.recommendWordNotInSet(setData, word.charAt(word.length() - 1));
+                    setData.add(wordBot);
+                    mWordAdapter.updateData(setData);
+                }
+            }
+        });
+    }
+
+    void findViewById() {
+        imgSupport = findViewById(R.id.imgSupport);
+        imgSend = findViewById(R.id.imgSend);
+        edtInputWord = findViewById(R.id.edtInputWord);
+        rvWord = findViewById(R.id.rvWord);
     }
 }
---
+++ app/src/main/java/com/xlteam/wordmatching/ui/play/WordAdapter.java
@@ -0,0 +1,87 @@
+package com.xlteam.wordmatching.ui.play;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.xlteam.wordmatching.R;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class WordAdapter extends RecyclerView.Adapter {
+    private final int TYPE_SENT = 0;
+    private final int TYPE_RECEIVED = 1;
+    private ArrayList<String> mWords;
+    private int maxUser;
+    private int numberUser;
+
+    WordAdapter(HashSet<String> words, int maxUser, int numberUser) {
+        this.maxUser = maxUser;
+        this.numberUser = numberUser;
+        mWords = new ArrayList<>();
+        mWords.addAll(words);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == 0) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_word_sent, parent, false);
+            return new SentViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_word_received, parent, false);
+            return new ReceivedViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder.getItemViewType() == TYPE_SENT) {
+            SentViewHolder sentViewHolder = (SentViewHolder) holder;
+            sentViewHolder.tvWord.setText(mWords.get(position));
+        } else {
+            ReceivedViewHolder receivedViewHolder = (ReceivedViewHolder) holder;
+            receivedViewHolder.tvWord.setText(mWords.get(position));
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position % maxUser == numberUser) return TYPE_SENT;
+        return TYPE_RECEIVED;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWords.size();
+    }
+
+    public void updateData(HashSet<String> sets) {
+        mWords = new ArrayList<>();
+        mWords.addAll(sets);
+        notifyDataSetChanged();
+    }
+
+    static class SentViewHolder extends RecyclerView.ViewHolder {
+        TextView tvWord;
+
+        SentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvWord = itemView.findViewById(R.id.tvWord);
+        }
+    }
+
+    static class ReceivedViewHolder extends RecyclerView.ViewHolder {
+        TextView tvWord;
+
+        ReceivedViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvWord = itemView.findViewById(R.id.tvWord);
+        }
+    }
+}
