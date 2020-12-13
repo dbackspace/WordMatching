44eca161c4046cd00a19bcafaaf5ee68d9017289
Duc Nguyen <42472693+dbackspace@users.noreply.github.com>
2020-12-13 22:17:49 +0700

    (Shelve) Test layout keyboard portrait

--- app/src/main/java/com/xlteam/wordmatching/ui/SplashActivity.java
+++ app/src/main/java/com/xlteam/wordmatching/ui/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.xlteam.wordmatching.ui.menu.MenuActivity;
 import com.xlteam.wordmatching.ui.playscreen.PlayActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -12,7 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, PlayActivity.class));
+        startActivity(new Intent(this, MenuActivity.class));
         finish();
     }
 }
